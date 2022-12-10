5db755cf8a34a32a80006c9f9639b708afaa9d41
Felipe F <feferreira.sf@gmail.com>
2022-12-10 20:30:40 -0300

    feat: home page mobile desktop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function App() {
   return (
-    <main className='w-full h-screen mx-auto px-6 pt-14 lg:px-[165px] flex flex-col lg:flex-row justify-center lg:justify-between items-center gap-8 lg:gap-0 home_background'>
+    <main className='w-full h-screen mx-auto px-6 pt-14 lg:px-[165px] flex flex-col lg:flex-row justify-center lg:justify-between items-center gap-4 lg:gap-0 home_background'>
       <div className='flex flex-col items-center lg:items-start gap-4 max-w-[450px] w-full'>
         <h2 className='md:text-[28px] tracking-widest md:tracking-[3.4px] lg:tracking-[4.7008px] font-barlowCond'>
           SO, YOU WANT TO TRAVEL TO
@@ -10,7 +10,7 @@ export default function App() {
         <h1 className='text-white font-belleFair text-[80px] md:text-[150px]'>
           SPACE
         </h1>
-        <p className='text-lg leading-6 text-center md:text-base md:leading-7 lg:leading-8 lg: lg:text-left'>
+        <p className='text-center md:text-lg lg:leading-8 lg: lg:text-left'>
           Let&apos;s face it; if you want to go to space, you might as well
           genuinely go to outer space and not hover kind of on the edge of it.
           Well sit back, and relax because we&apos;ll give you a truly out of
