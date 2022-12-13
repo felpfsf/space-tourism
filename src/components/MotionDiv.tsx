99a4c75ea0426e36187e71771b172da73743a374
Felipe F <feferreira.sf@gmail.com>
2022-12-13 14:52:59 -0300

    feat: framer motion component

---
+++ src/components/MotionDiv.tsx
@@ -0,0 +1,34 @@
+import { AnimatePresence, motion } from 'framer-motion'
+import { ReactNode } from 'react'
+
+interface IMotionDivProps {
+  children: ReactNode
+  motionKey: number
+}
+
+const MotionDiv = ({ children, motionKey }: IMotionDivProps) => {
+  const variations = {
+    enter: { opacity: 0, x: 0 },
+    visible: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: '-100vw' }
+  }
+
+  console.log(typeof motionKey)
+
+  return (
+    <AnimatePresence initial={false} mode='wait'>
+      <motion.div
+        variants={variations}
+        initial='enter'
+        animate='visible'
+        exit='exit'
+        key={motionKey}
+        transition={{ type: 'tween' }}
+        >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  )
+}
+
+export default MotionDiv
