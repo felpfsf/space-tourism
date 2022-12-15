2471ba9af5f8651bfaf57c64b4f92810dfc5c9d4
Felipe F <feferreira.sf@gmail.com>
2022-12-15 20:37:33 -0300

    style: crew pic size adjstments for hd res

--- src/pages/crew/components/CrewPic.tsx
+++ src/pages/crew/components/CrewPic.tsx
@@ -11,7 +11,7 @@ const CrewPic = ({ imgAlt, imgUrl, idx }: CrewPicProps) => {
   return (
     <>
       {/* <div className='h-56 my-6 w-44 md:my-14 place-self-center lg:place-self-start  md:w-[456px] md:h-[532px] border bg-blue-700'> */}
-      <div className='relative w-full h-56 my-6 place-self-center md:my-14 lg:my-0 md:h-[532px] lg:h-[712px] lg:place-self-start'>
+      <div className='relative w-full h-56 my-6 place-self-center md:my-14 lg:my-0 md:h-[532px] xl1:h-[660px] lg:place-self-start border-b-[1px] border-gray_900 lg:border-none'>
         <MotionDiv motionKey={idx}>
           <Image
             src={imgUrl}
